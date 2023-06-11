0c539d9dc59222e45f3cd7c911bee29ef9e246db
Vyas Giri <vyasgiri25@gmail.com>
2023-06-11 17:01:33 +0530

    Added a button to change font of the page

---
+++ index.js
@@ -0,0 +1,41 @@
+let button = document.getElementById('font_button');
+let vyas = document.getElementById('vyas');
+let lil = document.getElementsByClassName("lil");
+// let l1 = document.getElementById("l1");
+// let l2 = document.getElementById("l2");
+// let l3 = document.getElementById("l3");
+let para = document.getElementsByClassName("para");
+let heading = document.getElementById("heading");
+
+
+function font_change() {
+    if (button.style.fontFamily = 'cyberpunk') {
+        raleway();
+        everything_else_raleway();
+    }
+}
+
+// function cyberpunk() {
+//     button.style.fontFamily = "cyberpunk";
+// }
+
+function raleway() {
+    button.style.fontFamily = 'Raleway';
+}
+
+function everything_else_raleway() {
+    heading.style.fontFamily = 'Raleway';
+    para.style.fontFamily = 'Raleway';
+    vyas.style.fontFamily = 'Raleway';
+    lil.style.fontFamily = 'Raleway';
+    // l1.style.fontFamily = 'Raleway';
+    // l2.style.fontFamily = 'Raleway';
+    // l3.style.fontFamily = 'Raleway';    
+}
+
+// function everything_else_cyberpunk() {
+//     document.h1.style.fontFamily = 'cyberpunk';
+//     document.p.style.fontFamily = 'cyberpunk';
+//     vyas.style.fontFamily = 'cyberpunk';
+//     document.li.style.fontFamily = 'cyberpunk';
+// }
